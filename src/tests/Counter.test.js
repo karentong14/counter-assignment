2dd3f4ede610b83538eb30a8a05db5132c7d623a
karentong14 <karentong@ufl.edu>
2023-11-15 13:18:10 -0500

    fixed unit tests?

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
+import React from 'react'
+import {render, fireEvent, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter'
 // import necessary react testing library helpers here
 // import the Counter component here
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const countMessage = screen.getByText("Counter");
+  expect(countMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText('0')).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
+test('clicking + increments the count', async () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText('+'));
+  await screen.findByTestId('count');
+
+  expect(screen.getByText('1')).toBeInTheDocument();
 });
 
-test('clicking - decrements the count', () => {
+test('clicking - decrements the count', async () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText('-'));
+  await screen.findByTestId('count');
+
+  expect(screen.getByText('-1')).toBeInTheDocument();
 });
